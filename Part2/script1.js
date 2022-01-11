0647d3055cbc5327ccb1dddac905f6444f89e359
fraing31 <cisco120988@gmail.com>
2022-01-11 12:15:10 -0500

    I like me - javaScript

--- Part2/script1.js
+++ Part2/script1.js
@@ -4,12 +4,29 @@ let addLike2 = 9;
 
 let addLikeAll = document.querySelectorAll(".like");
 
+let span1 = document.querySelector("#NeLike");
+let span2 = document.querySelector("#NiLike");
+let span3 = document.querySelector("#jiLike");
+
+
+
 console.log(addLikeAll);
 
 function userLike() {
 
-    for( let k = 0; k < addLikeAll.length; k++ ) {
+    span1.innerText = parseInt(span1.textContent) + 1;
 
-        addLikeAll[k].innerText = addLikeAll + "like(s)";
-    
+}
+
+function userLike1() {
+
+    span2.innerText = parseInt(span2.textContent) + 1;
+
+}
+
+function userLike2() {
+
+    span3.innerText = parseInt(span3.textContent) + 1;
+
+
 }
